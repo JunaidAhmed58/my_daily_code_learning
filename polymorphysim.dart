9e2b473a59c42dfb8be082b917ae5288e4ba276a
JunaidAhmed58 <129215461+JunaidAhmed58@users.noreply.github.com>
2023-10-20 15:44:29 +0500

    polymorphsim

---
+++ polymorphysim.dart
@@ -0,0 +1,24 @@
+void main() {
+  // hr().fun1(); one way of call function
+  var obj = hr();
+  obj.fun1();
+  obj.name;
+}
+
+class emp {
+  String name = 'Ali';
+  fun1() {
+    print('emp_name : Is Junaid');
+  }
+}
+
+class hr extends emp {
+  String name = 'Haider';
+  fun1() {
+    print('Hr_name : Is Sara');
+    print(super.name);
+    print(name);
+
+    //  super.fun1();
+  }
+}
